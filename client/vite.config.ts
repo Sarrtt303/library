14ec7bc76f2f717e086dea903c2cbd45e3b15ba3
Sagar Debnath <sagardebnath1001@gmail.com>
2024-09-30 16:38:16 +0530

    vite config done

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This should match the publish directory in Netlify settings
+  },
+  base: '/', // Ensure that the base path is correct, especially if your app is served from a subdirectory
+});
